233ffad5f57c16af4785fe84ded5f3ea9bb9e29d
Denton Liu <liu.denton@gmail.com>
2020-04-03 21:11:34 -0400

    merge: teach --autostash option
    In rebase, one can pass the `--autostash` option to cause the worktree
    to be automatically stashed before continuing with the rebase. This
    option is missing in merge, however.
    
    Implement the `--autostash` option and corresponding `merge.autoStash`
    option in merge which stashes before merging and then pops after.
    
    This option is useful when a developer has some local changes on a topic
    branch but they realize that their work depends on another branch.
    Previously, they had to run something like
    
    	git fetch ...
    	git stash push
    	git merge FETCH_HEAD
    	git stash pop
    
    but now, that is reduced to
    
    	git fetch ...
    	git merge --autostash FETCH_HEAD
    
    When an autostash is generated, it is automatically reapplied to the
    worktree only in three explicit situations:
    
    	1. An incomplete merge is commit using `git commit`.
    	2. A merge completes successfully.
    	3. A merge is aborted using `git merge --abort`.
    
    In all other situations where the merge state is removed using
    remove_merge_branch_state() such as aborting a merge via
    `git reset --hard`, the autostash is saved into the stash reflog
    instead keeping the worktree clean.
    
    Suggested-by: Alban Gruin <alban.gruin@gmail.com>
    Signed-off-by: Denton Liu <liu.denton@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/reset.c
+++ builtin/reset.c
@@ -25,6 +25,7 @@
 #include "cache-tree.h"
 #include "submodule.h"
 #include "submodule-config.h"
+#include "sequencer.h"
 
 #define REFRESH_INDEX_DELAY_WARNING_IN_MS (2 * 1000)
 
@@ -437,8 +438,9 @@ int cmd_reset(int argc, const char **argv, const char *prefix)
 		if (reset_type == HARD && !update_ref_status && !quiet)
 			print_new_head_line(lookup_commit_reference(the_repository, &oid));
 	}
-	if (!pathspec.nr)
+	if (!pathspec.nr) {
 		remove_branch_state(the_repository, 0);
+	}
 
 	return update_ref_status;
 }
